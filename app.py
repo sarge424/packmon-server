56ede38a7d7dcb1511d7455adca34115cc29567c
Alex Dawson <arjunsivakumar2@gmail.com>
2023-07-13 01:05:41 +0530

    finished

--- app.py
+++ app.py
@@ -1,41 +1,52 @@
-from flask import Flask, render_template, request, redirect
-from flask_socketio import SocketIO, emit
+from flask import Flask, render_template, send_file
+from flask_socketio import SocketIO
+
+import numpy as np
+from PIL import Image
+import io
+
+from copy import deepcopy as dc
 
 app = Flask(__name__)
-socketio = SocketIO(app)
+socketio = SocketIO(app, max_http_buffer_size=50 * 1000 * 1000)
 events = []
-
+lastimg = []
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('index.html', events=events)
 
 
-@app.route('/generate')
-def form_fill():
-    return render_template('formfill.html')
-
-
-@app.route('/history')
-def show_history():
-    return render_template('history.html', events=events)
-
-
-@app.route('/event', methods=['POST'])
-def event():
-    data = (request.values.get('time'), request.values.get('duration'))
+@socketio.on('event')
+def add_data(data):
+    data['url'] = data['startTime'].replace(':', '')
     events.append(data)
-    socketio.emit('event', {'data': events})
-    print('emitted', data)
-    return redirect('/generate')
+    socketio.emit('log', {'data': events})
+    print('received an event', data['width'], data['height'])
 
+    w = data['width']
+    h = data['height']
+    img = np.array(data['frame']).reshape([h, w, 3])
 
-last_post = []
+    for j, row in enumerate(img):
+        for i, pixel in enumerate(row):
+            img[j][i] = [pixel[2], pixel[1], pixel[0]]
 
+    img = np.array(img)
+    img = Image.fromarray(img.astype('uint8'))
+    file_obj = io.BytesIO()
+    img.save(file_obj, 'PNG')
+    file_obj.seek(0)
 
-@app.route('/ping', methods=['GET', 'POST'])
-def ping_serv():
-    if request.method == 'POST':
-        last_post.append(request.values)
+    lastimg.append((data['startTime'], file_obj))
 
-    return str(last_post)
+@app.route('/img/<url>', methods=['GET'])
+def image_show(url):
+    for img in lastimg:
+        if img[0].replace(':', '') == url:
+            return send_file(dc(img[1]), mimetype='image/PNG')
+
+    return '404 - Not Found...'
+
+if __name__ == '__main__':
+    app.run(port=5007)
